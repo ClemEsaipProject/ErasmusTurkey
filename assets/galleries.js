71328fd02b07c470f84b6f449068e0e7d2681ddd
BlackCagouProd <blackcagou@outlook.fr>
2024-02-12 20:46:22 +0100

    add photo

--- assets/galleries.js
+++ assets/galleries.js
@@ -2,15 +2,35 @@ document.addEventListener("DOMContentLoaded", function() {
     const gallery = document.getElementById("gallery");
 
     const images = [{
+            src: "./img/galleries/20240204_145548.jpg",
+            alt: "Description de l'image 2",
+        }, {
             src: "./img/blog/20240210_153319.jpg",
             alt: "Description de l'image 1",
         },
+
         {
-            src: "./img/gallery/image2.jpg",
-            alt: "Description de l'image 2",
+            src: "./img/galleries/20240205_115531.jpg",
+            alt: "Description de l'image 3",
         },
         {
-            src: "./img/gallery/image3.jpg",
+            src: "./img/galleries/20240205_214954.jpg",
+            alt: "Description de l'image 3",
+        },
+        {
+            src: "./img/galleries/20240206_122356.jpg",
+            alt: "Description de l'image 3",
+        },
+        {
+            src: "./img/galleries/20240206_224229.jpg",
+            alt: "Description de l'image 3",
+        },
+        {
+            src: "./img/galleries/20240207_104817.jpg",
+            alt: "Description de l'image 3",
+        },
+        {
+            src: "./img/galleries/20240209_195613.jpg",
             alt: "Description de l'image 3",
         },
         // Ajoutez d'autres images ici
