df69e5ce7f3ef901dcdddbcaac1d28a50c850231
BlackCagouProd <blackcagou@outlook.fr>
2024-03-05 21:07:57 +0100

    add article 2

--- assets/article.js
+++ assets/article.js
@@ -3,13 +3,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const articles = [{
             title: "Discovering Tuzla:",
-            image: "./img/blog/20240213_155727.jpg",
+            image: ["./img/blog/20240213_155727.jpg"],
             text: `Istanbul's Maritime Gem Welcome to Tuzla, a coastal jewel nestled along the Marmara Sea, an integral part of Istanbul's fascinating urban tapestry in Turkey. Naval Industry and Maritime Economy: Tuzla shines particularly bright thanks to its dynamic naval industry. With its renowned shipyards, the city is the beating heart of boat and ship construction in Turkey. This flourishing activity extends beyond the local economy, significantly contributing to the national maritime economy. A Culture Deeply Connected to the Sea: Life in Tuzla is infused with the soothing influence of the sea. The local culture reflects this intimate connection with traditions rooted in fishing and navigation. Inhabitants celebrate their maritime heritage through festivals, events, and delightful cuisine featuring fresh seafood. History and Modernity Coexist: Tuzla also unveils its past through historical remnants dating back to the Byzantine and Ottoman eras. Captivating archaeological sites bear witness to the region's rich history, creating a striking contrast with the modernity of its shipyards and infrastructure. A Green and Blue Paradise: Despite its bustling industry, Tuzla offers green spaces and parks that invite relaxation. These verdant sanctuaries add a touch of serenity to residents' daily lives, providing peaceful retreats along the shore. In summary, Tuzla captivates with its harmonious blend of maritime tradition, captivating history, and a dynamic present geared toward the future. Explore its lively streets, savor its delicious cuisine, and let yourself be carried away by the enchanting charm of this exceptional maritime city.`
         },
         {
-            title: "Titre de l'article 2",
+            title: "Transport on Istanbul ",
+            image: "./img/blog/20240218_182307.jpg",
+            text: `Istanbul, a vast Turkish city divided into 40 districts, provides an integrated public transportation network managed by the rechargeable Istanbulkart. This card facilitates access to buses, the Marmaray (under the Bosphorus tunnel), the metro, and ferries, streamlining payments into a single method.
+
+            Buses form the backbone of the network, ensuring essential connectivity. The Marmaray offers a swift crossing under the Bosphorus, while the metro facilitates underground travel. Ferries offer a picturesque experience.
+            
+            The Istanbulkart, rechargeable and usable everywhere, eliminates the need to juggle different tickets. Fares remain affordable, with discounts for students and the elderly. In summary, Istanbul's transportation system, powered by the Istanbulkart, provides a variety of convenient options to explore the city, highlighting its efficiency and accessibility.`,
+        },
+        {
+            title: "Titre de l'article 3",
             image: "",
-            text: "Texte de l'article 2...",
+            text: "Texte de l'article 3...",
         },
         // Ajoutez d'autres articles ici
     ];
